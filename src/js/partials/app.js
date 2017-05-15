153d4c2d82b6bb5a42eb4f208aba897739de55ec
raduzhka <ya.raduzhka@gmail.com>
2017-05-15 12:07:08 +0300

    aboutBooks and img

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -6,20 +6,31 @@ $(window).on('load', function () {
         url: 'https://koskapanbooks.azurewebsites.net/api/Books',
         ContentType: 'application/json',
         success: function (dataBook) {
-            console.log(dataBook);
             for (var a in dataBook) {
+                var index_of_description_short = dataBook[a].description.substr(0, 220).lastIndexOf('.');
+                var description_short =  dataBook[a].description.substr(0, index_of_description_short.toNumber);
                 var template = $(".books").html();
-                console.log(template);
+                var description = mustache.render(template, description_short);
                 var donedata = mustache.render(template, dataBook[a]);
                 $('.books:first').clone().appendTo('#content').html(donedata);
-                // $('.books:last .titleBook').text(dataBook[a].title);
-                // $('.books:last .author span:last').text(dataBook[a].author);
-                // $('.books:last .pubYear span:last').text(dataBook[a].publicationYear);
-                // $('.books:last .description p').text(dataBook[a].description);
+                $('.books:last').children("div .img").css('background-image','url("'+dataBook[a].imageUrl+'")');
             }
         },
         error: function () {
             alert('fail');
         }
+    }),
+        $("#content").on("click", ".about_button", function() {
+        var idBook = $(this).closest('.books').children('.idBook').text();
+        $.ajax ({
+            type: 'GET',
+            url: 'https://koskapanbooks.azurewebsites.net/api/Books/{'+idBook+'}',
+            ContentType: 'application/json',
+            // data: idBook,
+            success: function (dataAboutBook) {
+                console.log(dataAboutBook);
+            }
+        })
     })
-});
+})
+
